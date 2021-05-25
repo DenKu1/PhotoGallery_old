3e990c8f91dfefd5e7a617764b0f4c59f71dc975
DenKu1 <ledar54@gmail.com>
2021-05-25 11:59:23 +0300

    Edit startup

--- PhotoGallery.API/Startup.cs
+++ PhotoGallery.API/Startup.cs
@@ -39,7 +39,27 @@ namespace PhotoGallery.API
             var jwtKey = Configuration["ApplicationSettings:JwtKey"];
             var connectionString = Configuration.GetConnectionString("PhotoGalleryConnection");
 
-            ConfigureAuthentication(services, jwtKey);
+            services.Configure<JwtSettings>(Configuration.GetSection("ApplicationSettings"));
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(opt =>
+            {
+                opt.RequireHttpsMetadata = false;
+                opt.SaveToken = false;
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
 
             services.Configure<IdentityOptions>(opt =>
             {
@@ -104,31 +124,6 @@ namespace PhotoGallery.API
                     spa.UseAngularCliServer(npmScript: "start");
                     //spa.UseProxyToSpaDevelopmentServer("http://localhost:52818");
                 }
-            });
-        }
-
-        protected virtual void ConfigureAuthentication(IServiceCollection services, string jwtKey)
-        {
-            services.Configure<JwtSettings>(Configuration.GetSection("ApplicationSettings"));
-            
-            services.AddAuthentication(opt =>
-            {
-                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(opt =>
-            {
-                opt.RequireHttpsMetadata = false;
-                opt.SaveToken = false;
-                opt.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                    ClockSkew = TimeSpan.Zero
-                };
             });
         }
     }
