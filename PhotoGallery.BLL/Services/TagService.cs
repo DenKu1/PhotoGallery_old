4512220f181719dcac41538770e6f52c79d034a7
DenKu1 <ledar54@gmail.com>
2021-12-28 01:38:02 +0200

    Add tags migrations

---
+++ PhotoGallery.BLL/Services/TagService.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using AutoMapper;
+
+using PhotoGallery.BLL.DTO.In;
+using PhotoGallery.BLL.DTO.Out;
+using PhotoGallery.BLL.Interfaces;
+using PhotoGallery.DAL.Entities;
+using PhotoGallery.DAL.Interfaces;
+
+namespace PhotoGallery.BLL.Services
+{
+    public class TagService : ITagService
+    {
+        IMapper mapper;
+        IUnitOfWork unitOfWork;
+        IServiceHelper helper;
+
+        public TagService(IMapper mapper, IUnitOfWork unitOfWork, IServiceHelper helper)
+        {
+            this.mapper = mapper;
+            this.unitOfWork = unitOfWork;
+            this.helper = helper;
+        }
+
+        public async Task AddTagsToPhotoAsync(IEnumerable<string> tagNames, int photoId)
+        {
+            var photo = await unitOfWork.Photos.GetByIdAsync(photoId);
+            helper.ThrowPhotoGalleryNotFoundExceptionIfModelIsNull(photo);
+
+            foreach (var tagName in tagNames)
+            {
+                var tag = await GetOrCreateTag(tagName);
+                
+                photo.Tags.Add(tag);
+            }
+
+            await unitOfWork.SaveAsync();
+        }
+
+        public async Task<IEnumerable<string>> GetTagsForPhotoAsync(int photoId)
+        {
+            var photo = await unitOfWork.Photos.GetByIdAsync(photoId);
+            helper.ThrowPhotoGalleryNotFoundExceptionIfModelIsNull(photo);
+
+            return photo.Tags.Select(tag => tag.Name);
+        }
+
+        public async Task AddTagsToUserAsync(IEnumerable<string> tagNames, int userId)
+        {
+            var user = await unitOfWork.Users.GetByIdAsync(userId);
+            helper.ThrowPhotoGalleryNotFoundExceptionIfModelIsNull(user);
+            
+            foreach (var tagName in tagNames)
+            {
+                var tag = await GetOrCreateTag(tagName);
+                
+                user.Tags.Add(tag);
+            }
+            
+            await unitOfWork.SaveAsync();
+        }
+
+        public async Task<IEnumerable<string>> GetTagsForUserAsync(int userId)
+        {
+            var user = await unitOfWork.Photos.GetByIdAsync(userId);
+            helper.ThrowPhotoGalleryNotFoundExceptionIfModelIsNull(user);
+
+            return user.Tags.Select(tag => tag.Name);
+        }
+
+        public async Task RemoveTagFromPhotoAsync(string tagName, int photoId)
+        {
+            var tag = await GetTagByName(tagName);
+            var photo = await unitOfWork.Photos.GetByIdAsync(photoId);
+            
+            helper.ThrowPhotoGalleryNotFoundExceptionIfModelIsNull(tag);
+            helper.ThrowPhotoGalleryNotFoundExceptionIfModelIsNull(photo);
+
+            photo.Tags.Remove(tag);
+
+            await unitOfWork.SaveAsync();
+        }
+
+        public async Task RemoveTagFromUserAsync(string tagName, int userId)
+        {
+            var tag = await GetTagByName(tagName);
+            var user = await unitOfWork.Users.GetByIdAsync(userId);
+            
+            helper.ThrowPhotoGalleryNotFoundExceptionIfModelIsNull(tag);
+            helper.ThrowPhotoGalleryNotFoundExceptionIfModelIsNull(user);
+
+            user.Tags.Remove(tag);
+
+            await unitOfWork.SaveAsync();
+        }
+
+        async Task<Tag> GetOrCreateTag(string tagName)
+        {
+            var tag = await GetTagByName(tagName);
+            
+            if (tag is null)
+            {
+                tag = new Tag { Name = tagName };
+                await unitOfWork.Tags.AddAsync(tag);
+            }
+
+            return tag;
+        }
+        
+        async Task<Tag> GetTagByName(string tagName)
+        {
+            return await unitOfWork.Tags.FindFirstOrDefaultAsync(tag => string.Equals(tag.Name, tagName));
+        }
+    }
+}
---
+++ PhotoGallery.DAL/Migrations/20211227142900_Add_tags.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PhotoGallery.DAL.Migrations
+{
+    public partial class Add_tags : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PhotoTag",
+                columns: table => new
+                {
+                    PhotosId = table.Column<int>(type: "int", nullable: false),
+                    TagsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhotoTag", x => new { x.PhotosId, x.TagsId });
+                    table.ForeignKey(
+                        name: "FK_PhotoTag_Photos_PhotosId",
+                        column: x => x.PhotosId,
+                        principalTable: "Photos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PhotoTag_Tags_TagsId",
+                        column: x => x.TagsId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TagUser",
+                columns: table => new
+                {
+                    TagsId = table.Column<int>(type: "int", nullable: false),
+                    UsersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TagUser", x => new { x.TagsId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_TagUser_AspNetUsers_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TagUser_Tags_TagsId",
+                        column: x => x.TagsId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: -2,
+                column: "ConcurrencyStamp",
+                value: "e7c2b6eb-2bd4-45b4-890e-c227199d8f4a");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: -1,
+                column: "ConcurrencyStamp",
+                value: "b185aaaf-506d-451a-844f-d3d4505e1c29");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: -1,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "3e119f8f-f914-47f5-a8b8-bd221f38d1f4", "AQAAAAEAACcQAAAAEHSw0F48/iKAV1+h9gXl6SdR9TkYuKe5PLTsN+1LfxDHvdIjjG7t9ryDvNmjxcXL7Q==" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PhotoTag_TagsId",
+                table: "PhotoTag",
+                column: "TagsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TagUser_UsersId",
+                table: "TagUser",
+                column: "UsersId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PhotoTag");
+
+            migrationBuilder.DropTable(
+                name: "TagUser");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: -2,
+                column: "ConcurrencyStamp",
+                value: "86f969d0-1489-4f09-92f1-8ccadf9bd583");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: -1,
+                column: "ConcurrencyStamp",
+                value: "d5704e83-a363-4940-9e96-c97abf3bd2fa");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: -1,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "bf16b325-04af-4966-9924-53016d5e60ef", "AQAAAAEAACcQAAAAEEsSZRtavc+6CeyIOVDL+7QlViRqNGj1HZ4bQZ9mfnUweLAB1WonBZbp949j3xJbeg==" });
+        }
+    }
+}
